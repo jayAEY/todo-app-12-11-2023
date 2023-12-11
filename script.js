c27c76c103a8b715889c947f859315d8f138220f
jayAEY <106685702+jayAEY@users.noreply.github.com>
2023-12-11 15:33:26 -0800

    started work on localstorage of tasks

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 //search
 //edit
 //delete
+
 let taskInput = document.querySelector("#task-input");
 let searchInput = document.querySelector("search-input");
 
@@ -11,19 +12,44 @@ let searchButton = document.querySelector("#search");
 let deleteButton = document.querySelector("#delete");
 
 let todosection = document.querySelector("#todo-section");
+let list = document.querySelector("#list");
 
 let tasks = localStorage.getItem("tasks") || [];
 
-localStorage.getItem("tasks");
+// console.log(tasks);
+
+console.log(addButton);
+
+function addTask(btn) {
+  let task = btn.previousElementSibling.value;
+
+  let newTaskHTML = `<li class="task">
+                        ${task}
+                        <input
+                        class="taskcheckbox"
+                        type="checkbox"
+                        />
+                        </li>`;
+
+  if (task != "") {
+    todosection.style.display = "block";
+    tasks += task;
+    localStorage.setItem("tasks", tasks);
+    // console.log(list.innerHTML);
+    list.innerHTML += newTaskHTML;
+
+    console.log(localStorage.getItem("tasks"));
+  }
+  //   if (task == "") {
+  //     btn.previousElementSibling
+  //   }
+  //   console.log(task);
+}
+
+addButton.addEventListener("click", (e) => addTask(e.target));
+
+// localStorage.getItem("tasks");
 
 // #input-section input {
-
-// #search,
-// #add,
-// #delete
-
-// #todo-section
-
-// #list
 
 // .task
