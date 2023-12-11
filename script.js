42c193ea94c2d7ce5cf98c54ebfc5f379d39d9e2
jayAEY <106685702+jayAEY@users.noreply.github.com>
2023-12-11 15:01:30 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//features:
+//add
+//search
+//edit
+//delete
+let taskInput = document.querySelector("#task-input");
+let searchInput = document.querySelector("search-input");
+
+let addButton = document.querySelector("#add");
+let searchButton = document.querySelector("#search");
+let deleteButton = document.querySelector("#delete");
+
+let todosection = document.querySelector("#todo-section");
+
+let tasks = localStorage.getItem("tasks") || [];
+
+localStorage.getItem("tasks");
+
+// #input-section input {
+
+// #search,
+// #add,
+// #delete
+
+// #todo-section
+
+// #list
+
+// .task
