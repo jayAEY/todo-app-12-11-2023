80ad5e4046f942ff14f4cff67711018887af2409
jayAEY <106685702+jayAEY@users.noreply.github.com>
2023-12-15 10:00:08 -0800

    add task functionality working

--- script.js
+++ script.js
@@ -16,67 +16,48 @@ let list = document.querySelector("#list");
 
 let tasks = [];
 
-// if (localStorage.getItem("tasks")) {
-//   tasks = localStorage.getItem("tasks").split(",");
-// }
-
 function renderTasks() {
-  // todosection.style.display = "block";
-
   if (localStorage.getItem("tasks")) {
     tasks = localStorage.getItem("tasks").split(",");
   }
-
   if (tasks.length > 0) {
     todosection.style.display = "block";
-
-    console.log(tasks);
   }
+  tasksHTML();
+}
 
+window.onload = renderTasks();
+
+function tasksHTML() {
+  let newTaskHTML = "";
   tasks.forEach((task) => {
-    let newTaskHTML = `<li class="task">
+    let addHTML = `<li class="task">
                      ${task}
                     <input
                     class="taskcheckbox"
                     type="checkbox"
                     />
                     </li>`;
-    list.innerHTML += newTaskHTML;
+    newTaskHTML += addHTML;
   });
+  list.innerHTML = newTaskHTML;
 }
-
-window.onload = renderTasks();
-
-// renderTasks();
-
-// console.log(tasks);
 
 function addTask(btn) {
   let task = btn.previousElementSibling.value;
-
-  if (task != "") {
+  if (task != "" && task.indexOf(",") === -1) {
     todosection.style.display = "block";
     tasks.push(task);
     localStorage.setItem("tasks", tasks);
-
-    // console.log(localStorage.getItem("tasks"));
-    // list.innerHTML += newTaskHTML;
   }
-
   renderTasks();
-  //   if (task == "") {
-  //     btn.previousElementSibling
-  //   }
-  //   console.log(task);
 }
 
+taskInput.addEventListener("invalid", () => {
+  taskInput.setCustomValidity("Please fill out this field (No commas)");
+});
 addButton.addEventListener("click", (e) => {
   addTask(e.target);
 });
 
 window.onload = renderTasks();
-// localStorage.getItem("tasks");
-
-// #input-section input {
-
-// .task
