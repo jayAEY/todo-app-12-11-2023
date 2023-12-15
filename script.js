a580811bcee1c98d465b76a4d314fa94e7cfcfae
jayAEY <106685702+jayAEY@users.noreply.github.com>
2023-12-15 13:38:20 -0800

    search function finished

--- script.js
+++ script.js
@@ -12,9 +12,11 @@ let searchButton = document.querySelector("#search");
 let deleteButton = document.querySelector("#delete");
 
 let todosection = document.querySelector("#todo-section");
-let list = document.querySelector("#list");
+let taskList = document.querySelector("#taskList");
+let searchList = document.querySelector("#searchList");
 
 let tasks = [];
+let newTaskHTML = "";
 
 function renderTasks() {
   if (localStorage.getItem("tasks")) {
@@ -29,9 +31,9 @@ function renderTasks() {
 window.onload = renderTasks();
 
 function tasksHTML() {
-  let newTaskHTML = "";
+  newTaskHTML = "";
   tasks.forEach((task) => {
-    let addHTML = `<li class="task">
+    let addHTML = `<li class="task" id="${task}">
                      ${task}
                     <input
                     class="taskcheckbox"
@@ -40,24 +42,45 @@ function tasksHTML() {
                     </li>`;
     newTaskHTML += addHTML;
   });
-  list.innerHTML = newTaskHTML;
+  taskList.innerHTML = newTaskHTML;
 }
 
 function addTask(btn) {
-  let task = btn.previousElementSibling.value;
-  if (task != "" && task.indexOf(",") === -1) {
-    todosection.style.display = "block";
-    tasks.push(task);
-    localStorage.setItem("tasks", tasks);
-  }
+  let task = btn.previousElementSibling.value.trim();
+  todosection.style.display = "block";
+  tasks.push(task);
+  localStorage.setItem("tasks", tasks);
+  taskList.style.display = "flex";
+  searchList.style.display = "none";
   renderTasks();
 }
 
-taskInput.addEventListener("invalid", () => {
-  taskInput.setCustomValidity("Please fill out this field (No commas)");
-});
+function searchTask(btn) {
+  let search = btn.previousElementSibling.value.trim();
+  if (search === "") {
+    taskList.style.display = "flex";
+    searchList.style.display = "none";
+  } else {
+    let searchHTML = "";
+    taskList.childNodes.forEach((task) => {
+      if (task.innerText.includes(search)) {
+        searchHTML += task.outerHTML;
+      }
+    });
+    // taskList.innerHTML = searchHTML;'
+    taskList.style.display = "none";
+    searchList.style.display = "flex";
+    searchList.innerHTML = searchHTML;
+  }
+}
+
 addButton.addEventListener("click", (e) => {
-  addTask(e.target);
+  if (taskInput.value.indexOf(",") !== -1 || taskInput.value.trim() === "") {
+    taskInput.setCustomValidity("Please fill out this field (No commas)");
+  } else {
+    addTask(e.target);
+  }
 });
+searchButton.addEventListener("click", (e) => searchTask(e.target));
 
 window.onload = renderTasks();
